66a2cd15e2471be8626c872450fc50ca226df25d
furyu <furyutei@gmail.com>
2020-03-03 20:57:56 +0900

    0.0.3: 「その他のツイート」を見たい時用にブラウザバック可能なように修正

--- src/js/MoreTweetsBuster.user.js
+++ src/js/MoreTweetsBuster.user.js
@@ -1,19 +1,35 @@
 // ==UserScript==
 // @name         MoreTweetsBuster
 // @namespace    https://furyutei.work
-// @version      0.0.2
+// @version      0.0.3
 // @description  Turn off the "More Tweets" display when open individual tweet
 // @author       furyu
 // @match        https://*.twitter.com/*/status/*
 // @grant        none
 // ==/UserScript==
 
-( () => {
+( ( SCRIPT_NAME ) => {
 'use strict';
 
+const
+    touched_name = SCRIPT_NAME + '_touched';
+
+let redirect_url;
+
+if ( window.name == touched_name ) {
+    return;
+}
+
 if ( location.href.match( /^(.*)\?(?:.+&)?ref_src=/ ) ) {
+    window.name = touched_name;
     // [メモ]クエリに "ref_src=～" が含まれていると「その他のツイート」が表示される→クエリ無しURLにリダイレクト
-    location.replace( RegExp.$1 );
+    redirect_url = RegExp.$1;
+    //location.replace( redirect_url );
+    // [メモ]遷移元の「その他のツイート」を見たいケースも有る模様→ history に残して『戻る』ことができるようにしておく
+    // TODO: 即遷移すると history に残らないケースあり？→遷移を遅らせる
+    setTimeout( () => {
+        location.href = redirect_url;
+    }, 100 );
     return;
 }
 
@@ -22,7 +38,7 @@ if ( ( ! document.referrer ) || ( new URL( document.referrer ).host == new URL( 
 }
 
 // [メモ]リファラが twitter.com 以外の場合、「その他のツイート」が表示される→ document.referrer が twitter.com になるようにリダイレクト
-//location.reload(); // Firefox だと document.referrer は変わらない
-location.replace( location.href );
+redirect_url = location.href.replace( /\?.*$/, '' ) + '?ref_src=' + encodeURIComponent( document.referrer );
+location.replace( redirect_url );
 
-} )();
+} )( 'MoreTweetsBuster' );
