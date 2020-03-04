0c2116207cad8ce85a75d7323e241ed3e07d4e97
furyu <furyutei@gmail.com>
2020-03-04 12:05:59 +0900

    0.0.4: ページ遷移方法変更

--- src/js/MoreTweetsBuster.user.js
+++ src/js/MoreTweetsBuster.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MoreTweetsBuster
 // @namespace    https://furyutei.work
-// @version      0.0.3
+// @version      0.0.4
 // @description  Turn off the "More Tweets" display when open individual tweet
 // @author       furyu
 // @match        https://*.twitter.com/*/status/*
@@ -12,33 +12,56 @@
 'use strict';
 
 const
-    touched_name = SCRIPT_NAME + '_touched';
+    TOUCHED_NAME = SCRIPT_NAME + '_touched',
+    ATTEMPT_PAGE_TRANSITION_TIMEOUT = 15000;
 
-let redirect_url;
-
-if ( window.name == touched_name ) {
+if ( window.name == TOUCHED_NAME ) {
     return;
 }
 
-if ( location.href.match( /^(.*)\?(?:.+&)?ref_src=/ ) ) {
-    window.name = touched_name;
-    // [メモ]クエリに "ref_src=～" が含まれていると「その他のツイート」が表示される→クエリ無しURLにリダイレクト
-    redirect_url = RegExp.$1;
-    //location.replace( redirect_url );
-    // [メモ]遷移元の「その他のツイート」を見たいケースも有る模様→ history に残して『戻る』ことができるようにしておく
-    // TODO: 即遷移すると history に残らないケースあり？→遷移を遅らせる
-    setTimeout( () => {
-        location.href = redirect_url;
-    }, 100 );
+// [メモ] 以下のいずれかの条件で、個別ツイート（https://twitter.com/<screen-name>/status/<tweet-id>）表示時に「その他のツイート」が表示される模様
+// - クエリに "ref_src=～" を含む
+// - リファラが twitter.com 以外
+
+if (
+    ( ! /\?(?:.+&)?ref_src=/.test( location.href ) ) &&
+    ( ( ! document.referrer ) || ( new URL( document.referrer ).host == new URL( location.href ).host ) )
+) {
     return;
 }
 
-if ( ( ! document.referrer ) || ( new URL( document.referrer ).host == new URL( location.href ).host ) ) {
+if ( ! location.href.match( /^https:\/\/[^/]+\/[^/]+\/status\/(\d+)/ ) ) {
     return;
 }
 
-// [メモ]リファラが twitter.com 以外の場合、「その他のツイート」が表示される→ document.referrer が twitter.com になるようにリダイレクト
-redirect_url = location.href.replace( /\?.*$/, '' ) + '?ref_src=' + encodeURIComponent( document.referrer );
-location.replace( redirect_url );
+let target_tweet_id = RegExp.$1;
+
+window.name = TOUCHED_NAME;
+
+let attempt_page_transition = () => {
+        let target_link_time = document.querySelector( 'a[href$="/status/' + target_tweet_id + '"] > time' ),
+            target_link = target_link_time && target_link_time.parentNode;
+        
+        if ( ! target_link ) {
+            return;
+        }
+        
+        clearTimeout( giveup_timer_id );
+        observer.disconnect();
+        
+        target_link.click()
+    },
+    
+    observer = new MutationObserver( ( records ) => {
+        attempt_page_transition();
+    } ),
+    
+    giveup_timer_id = setTimeout( () => {
+        observer.disconnect();
+    }, ATTEMPT_PAGE_TRANSITION_TIMEOUT );
+
+observer.observe( document, { childList: true, subtree: true } );
+
+attempt_page_transition();
 
 } )( 'MoreTweetsBuster' );
