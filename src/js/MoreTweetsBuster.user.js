e7728eb0312417d78f803a6afca1898faec88e24
furyu <furyutei@gmail.com>
2021-09-20 16:05:59 +0900

    0.0.11: 動作しなくなっていたため処理を見直し

--- src/js/MoreTweetsBuster.user.js
+++ src/js/MoreTweetsBuster.user.js
@@ -3,7 +3,7 @@
 // @name:ja         『その他のツイート』排除
 // @namespace       https://furyutei.work
 // @license         MIT
-// @version         0.0.10
+// @version         0.0.11
 // @description     Turn off the "More Tweets" display when open individual tweet
 // @description:ja  個別ツイートを開いた際の『その他のツイート』表示を抑制
 // @author          furyu
@@ -20,7 +20,7 @@
 
 const
     TOUCHED_NAME = SCRIPT_NAME + '_touched',
-    ATTEMPT_PAGE_TRANSITION_TIMEOUT = 15000;
+    USER_AGENT = window.navigator.userAgent.toLowerCase();
 
 if ( window.name == TOUCHED_NAME ) {
     return;
@@ -30,7 +30,6 @@ if ( window.name == TOUCHED_NAME ) {
 // - クエリに "ref_src=～" を含む
 // - [クエリに "s=20" を含む](https://twitter.com/esperecyan/status/1239330675897049090)
 // - リファラが twitter.com 以外
-
 if ( ! (
     ( /\?(?:.+&)?(?:ref_src|s)=/.test( location.href ) ) ||
     ( document.referrer && ( new URL( document.referrer ).host != new URL( location.href ).host ) )
@@ -38,81 +37,62 @@ if ( ! (
     return;
 }
 
-let parse_status_url = ( url ) => {
-        let match_result = new URL( url, location.href ).pathname.match( /^\/([^/]+)\/status\/(\d+)/ );
+const
+    parse_status_url = ( url ) => {
+        const
+            match_result = new URL( url, location.href ).pathname.match( /^\/([^/]+)\/status\/(\d+)/ );
         
-        return ( match_result ) ? { screen_name : match_result[ 1 ], tweet_id : match_result[ 2 ] } : {};
+        if ( ! match_result ) return {};
+        
+        return {
+            screen_name : match_result[ 1 ],
+            tweet_id : match_result[ 2 ]
+        };
     },
-    
     location_status_info = parse_status_url( location.href );
 
 if ( ! location_status_info.tweet_id ) {
     return;
 }
 
-let location_tweet_id = location_status_info.tweet_id;
+try {
+    window.name = TOUCHED_NAME;
+}
+catch ( error ) {
+    console.info( 'window.name could not be changed', error );
+}
 
-window.name = TOUCHED_NAME;
-
-let attempt_page_transition = () => {
-        if ( 0 < window.pageYOffset ) {
-            // TODO: スクロールされていたら一番上のツイートかどうかの判定が困難
-            // →暫定的に、何もせずに終了
-            stop_observer();
-            return;
+const
+    do_page_transition = ( url ) => {
+        if ( ! url ) return;
+        
+        // TODO: ページ読み込みを発生させないために pushState を使用しているが、Twitter側で state の構造（特に key）が変わってしまうとうまく動かなくなる
+        try {
+            const
+                state = {
+                    key : 'r80bpk',
+                    state : {
+                        fromApp : true,
+                        previousPath : location.pathname,
+                    },
+                },
+                pop_state_event = new PopStateEvent( 'popstate', { state : state } );
+            
+            history.pushState( state, '', new URL( url ).pathname );
+            dispatchEvent( pop_state_event );
         }
-        
-        let target_link_time = document.querySelector( 'a[href$="/status/' + location_tweet_id + '"] > time' ),
-            target_link = target_link_time && target_link_time.parentNode;
-        
-        if ( ! target_link ) {
-            // [メモ] Twilog 等から https://twitter.com/<retweeter>/status/<retweet-id> のように開かれると、retweet-id に一致するようなリンクが存在しない
-            // →一番上のツイートが a > time を含むかどうかで判定
-            let first_article = document.querySelector( 'div[data-testid="primaryColumn"] section[role="region"] article[role="article"]' );
-            
-            if ( ! first_article ) {
-                return;
-            }
-            
-            target_link_time = first_article.querySelector( 'a[role="link"][href*="/status/"] > time' );
-            target_link = target_link_time && target_link_time.parentNode;
-            
-            if ( ( ! target_link ) || ( ! parse_status_url( target_link.href ).tweet_id ) ) {
-                return;
-            }
+        catch ( error ) {
+            location.href = url;
         }
-        
-        stop_observer();
-        
-        /*
-        //if ( parse_status_url( target_link.href ).tweet_id != location_tweet_id ) {
-        //    // TODO: 可能ならリツイートURLのままで「その他のツイート」を非表示にしたい（historyに残るような形でうまく遷移できない）
-        //    location.href = '/' + location_status_info.screen_name + '/status/' + location_tweet_id;
-        //    return;
-        //}
-        */
-        target_link.click();
     },
-    
-    observer = new MutationObserver( ( records ) => {
-        attempt_page_transition();
-    } ),
-    
-    stop_observer = () => {
-        if ( giveup_timer_id ) {
-            clearTimeout( giveup_timer_id );
-            giveup_timer_id = null;
-        }
-        observer.disconnect();
-    },
-    
-    giveup_timer_id = setTimeout( () => {
-        giveup_timer_id = null;
-        stop_observer();
-    }, ATTEMPT_PAGE_TRANSITION_TIMEOUT );
+    transition_url = new URL( `/${location_status_info.screen_name}/status/${location_status_info.tweet_id}`, location.href ).href;
 
-observer.observe( document, { childList: true, subtree: true } );
+if ( USER_AGENT.indexOf('firefox') != -1 ) {
+    // TODO: Firefoxの場合、pushStateによる遷移では「その他のツイート」表示が消えない
+    location.href = transition_url;
+    return;
+}
 
-attempt_page_transition();
+do_page_transition( transition_url );
 
 } )( 'MoreTweetsBuster' );
